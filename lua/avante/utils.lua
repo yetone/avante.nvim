4bc74da78cdee97c8b7309ae270d909382037f2f
yetone <yetoneful@gmail.com>
2024-08-15 16:01:56 +0800

    Revert "fix: escape original_content" (#8)
    This reverts commit 518e0858a066a4ee6f2414f5e457314db19d8023.

--- lua/avante/utils.lua
+++ lua/avante/utils.lua
@@ -4,8 +4,4 @@ function M.trim_suffix(str, suffix)
   return string.gsub(str, suffix .. "$", "")
 end
 
-function M.escape(str)
-  return string.gsub(str, "([%(%)%.%%%+%-%*%?%[%^%$%]])", "%%%1")
-end
-
 return M
