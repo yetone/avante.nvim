3db158036c83130bdf702bc2e6b318afdc4b4c14
yuchanns <me@yuchanns.xyz>
2024-08-22 17:36:34 +0800

    fix(sidebar): invalid window number

--- lua/avante/sidebar.lua
+++ lua/avante/sidebar.lua
@@ -586,7 +586,11 @@ function Sidebar:on_mount()
   api.nvim_create_autocmd("WinResized", {
     group = self.augroup,
     callback = function()
-      if not api.nvim_win_is_valid(self.input_container.winid) or not api.nvim_win_is_valid(self.input.winid) then
+      if
+        not self.input_container.winid
+        or not api.nvim_win_is_valid(self.input_container.winid)
+        or not api.nvim_win_is_valid(self.input.winid)
+      then
         return
       end
 
