6ecd6280520664e2f73850c8d8eaad5a7adbc41f
yetone <yetoneful@gmail.com>
2024-09-03 22:10:54 +0800

    fix(ci): pre built tar.gz (#488)

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-set -eo pipefail
-
-# Check if jq is installed
-if ! command -v jq &>/dev/null; then
-  echo "Error: jq is not installed. Please install jq."
-  exit 1
-fi
-
-# Check if GITHUB_TOKEN is set
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "Error: GITHUB_TOKEN is not set. Please provide a valid GitHub token."
-  exit 1
-fi
+set -xeo pipefail
 
 REPO_OWNER="yetone"
 REPO_NAME="avante.nvim"
@@ -21,12 +9,6 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Set the target directory to clone the artifact
 TARGET_DIR="${SCRIPT_DIR}/build"
-
-# Get the latest successful run ID of the workflow
-RUN_ID=$(curl -s \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/workflows/build.yaml/runs?status=success&branch=main&per_page=1" | jq ".workflow_runs[0].id")
 
 # Get the artifact download URL based on the platform and Lua version
 case "$(uname -s)" in
@@ -52,12 +34,8 @@ LUA_VERSION="${LUA_VERSION:-luajit}"
 ARTIFACT_NAME_PATTERN="avante_lib-$PLATFORM-latest-$LUA_VERSION"
 
 # Get the artifact download URL
-ARTIFACT_URL=$(curl -s \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/artifacts" |
-  jq -r '.artifacts[] | select(.name | test("'"$ARTIFACT_NAME_PATTERN"'")) | .archive_download_url')
+ARTIFACT_URL=$(curl -s "https://api.github.com/repos/yetone/avante.nvim/releases/latest" | grep "browser_download_url" | cut -d '"' -f 4 | grep $ARTIFACT_NAME_PATTERN)
 
 mkdir -p "$TARGET_DIR"
 
-curl -L -H "Authorization: Bearer $GITHUB_TOKEN" "$ARTIFACT_URL" | tar -x -C "$TARGET_DIR"
+curl -L "$ARTIFACT_URL" | tar -zxv -C "$TARGET_DIR"
