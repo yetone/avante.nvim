cf4d70fc9ce9862ed1131ec37f9a06f0b7517473
GINTOAHC <chaotingchen10@gmail.com>
2024-09-04 19:27:28 +0800

    style

--- lua/avante/path.lua
+++ lua/avante/path.lua
@@ -140,7 +140,7 @@ P.available = function() return templates ~= nil end
 
 P.clear = function()
   P.cache_path:rm({ recursive = true })
-	P.history_path:rm({recursive = true})
+  P.history_path:rm({ recursive = true })
 end
 
 return P
