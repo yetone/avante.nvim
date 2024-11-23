c0da171ac3bfa22e7d2be55a0dcfe67d2bfc3a6c
yetone <yetoneful@gmail.com>
2024-11-24 05:10:09 +0800

    fix: diff insert (#893)

--- lua/avante/sidebar.lua
+++ lua/avante/sidebar.lua
@@ -462,6 +462,10 @@ local function insert_conflict_contents(bufnr, snippets)
 
   for _, snippet in ipairs(snippets) do
     local start_line, end_line = unpack(snippet.range)
+    if start_line > end_line then
+      start_line = start_line + 1
+      end_line = end_line + 1
+    end
 
     local need_prepend_indentation = false
     local start_line_indentation = ""
