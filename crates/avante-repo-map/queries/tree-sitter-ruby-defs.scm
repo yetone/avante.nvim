0a837a4583d66abaf85c9d31f5efad12af87c736
Aaron Lifton <alifton@synack.com>
2025-01-15 05:37:53 -0500

    feat: improve ruby parsing (#1085)

--- crates/avante-repo-map/queries/tree-sitter-ruby-defs.scm
+++ crates/avante-repo-map/queries/tree-sitter-ruby-defs.scm
@@ -1,16 +1,26 @@
 ;; Capture top-level methods, class definitions, and methods within classes
-(program
-  (class
-    (body_statement
-      (call) @class_call
-      (assignment) @class_assignment
-      (method) @method
-    )
-  ) @class
-)
+
+(class
+  (body_statement
+    (call)? @class_call
+    (assignment)? @class_assignment
+    (method)? @method
+  )
+) @class
+
 (program
   (method) @function
 )
 (program
   (assignment) @assignment
+)
+
+(module) @module
+
+(module
+  (body_statement
+    (call)? @class_call
+    (assignment)? @class_assignment
+    (method)? @method
+  )
 )
