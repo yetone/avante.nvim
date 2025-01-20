36a290c28a9b27b10a3f55906327dfaddd1c8332
yetone <yetoneful@gmail.com>
2025-01-21 00:04:48 +0800

    fix: release the cursor when generating (#1109)

--- lua/avante/config.lua
+++ lua/avante/config.lua
@@ -114,13 +114,16 @@ M._defaults = {
     timeout = 60000, -- Timeout in milliseconds
   },
   ---Specify the behaviour of avante.nvim
-  ---1. auto_apply_diff_after_generation: Whether to automatically apply diff after LLM response.
+  ---1. auto_focus_sidebar              : Whether to automatically focus the sidebar when opening avante.nvim. Default to true.
+  ---2. auto_suggestions = false, -- Whether to enable auto suggestions. Default to false.
+  ---3. auto_apply_diff_after_generation: Whether to automatically apply diff after LLM response.
   ---                                     This would simulate similar behaviour to cursor. Default to false.
-  ---2. auto_set_keymaps                : Whether to automatically set the keymap for the current line. Default to true.
+  ---4. auto_set_keymaps                : Whether to automatically set the keymap for the current line. Default to true.
   ---                                     Note that avante will safely set these keymap. See https://github.com/yetone/avante.nvim/wiki#keymaps-and-api-i-guess for more details.
-  ---3. auto_set_highlight_group        : Whether to automatically set the highlight group for the current line. Default to true.
-  ---4. support_paste_from_clipboard    : Whether to support pasting image from clipboard. This will be determined automatically based whether img-clip is available or not.
-  ---5. minimize_diff                   : Whether to remove unchanged lines when applying a code block
+  ---5. auto_set_highlight_group        : Whether to automatically set the highlight group for the current line. Default to true.
+  ---6. jump_to_result_buffer_on_finish = false, -- Whether to automatically jump to the result buffer after generation
+  ---7. support_paste_from_clipboard    : Whether to support pasting image from clipboard. This will be determined automatically based whether img-clip is available or not.
+  ---8. minimize_diff                   : Whether to remove unchanged lines when applying a code block
   behaviour = {
     auto_focus_sidebar = true,
     auto_suggestions = false, -- Experimental stage
@@ -128,6 +131,7 @@ M._defaults = {
     auto_set_highlight_group = true,
     auto_set_keymaps = true,
     auto_apply_diff_after_generation = false,
+    jump_result_buffer_on_finish = false,
     support_paste_from_clipboard = false,
     minimize_diff = true,
   },
