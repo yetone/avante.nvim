3eaaaa8f5fcb9ee617ac425a87a156eeb3b6642c
miguelosana <miguelosana@gmail.com>
2025-03-10 15:43:10 +0100

    feat: add vertex claude provider (#1549)
    * feat: Add vertex claude provider
    
    * remove debug logging
    
    ---------
    
    Co-authored-by: Miguelo Sana <miguelo@incubeta.com>

---
+++ lua/avante/providers/vertex_claude.lua
@@ -0,0 +1,59 @@
+local P = require("avante.providers")
+local Vertex = require("avante.providers.vertex")
+
+---@class AvanteProviderFunctor
+local M = {}
+
+M.role_map = {
+  user = "user",
+  assistant = "assistant",
+}
+
+M.is_disable_stream = P.claude.is_disable_stream
+M.parse_messages = P.claude.parse_messages
+M.parse_response = P.claude.parse_response
+M.parse_api_key = Vertex.parse_api_key
+M.on_error = Vertex.on_error
+
+Vertex.api_key_name = "cmd:gcloud auth print-access-token"
+
+---@param prompt_opts AvantePromptOptions
+function M:parse_curl_args(prompt_opts)
+  local provider_conf, request_body = P.parse_config(self)
+  local location = vim.fn.getenv("LOCATION")
+  local project_id = vim.fn.getenv("PROJECT_ID")
+  local model_id = provider_conf.model or "default-model-id"
+  if location == nil or location == vim.NIL then location = "default-location" end
+  if project_id == nil or project_id == vim.NIL then project_id = "default-project-id" end
+  local url = provider_conf.endpoint:gsub("LOCATION", location):gsub("PROJECT_ID", project_id)
+
+  url = string.format("%s/%s:streamRawPredict", url, model_id)
+
+  local system_prompt = prompt_opts.system_prompt or ""
+  local messages = self:parse_messages(prompt_opts)
+  request_body = vim.tbl_deep_extend("force", request_body, {
+    anthropic_version = "vertex-2023-10-16",
+    temperature = 0,
+    max_tokens = 4096,
+    stream = true,
+    messages = messages,
+    system = {
+      {
+        type = "text",
+        text = system_prompt,
+        cache_control = { type = "ephemeral" },
+      },
+    },
+  })
+
+  return {
+    url = url,
+    headers = {
+      ["Authorization"] = "Bearer " .. Vertex.parse_api_key(),
+      ["Content-Type"] = "application/json; charset=utf-8",
+    },
+    body = vim.tbl_deep_extend("force", {}, request_body),
+  }
+end
+
+return M
