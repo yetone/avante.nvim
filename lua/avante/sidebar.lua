120aa3df22b3549e771ccfa6cc80107e55504fca
Markus Koller <markus@snafu.ch>
2025-03-15 12:31:27 +0100

    fix: don't rebalance windows after opening/closing the sidebar
    The `wincmd =` call in `Sidebar:open()` overrides the widths/heights of
    the sidebar windows that were previously set. This only happens in
    certain situations though for some reason, e.g. when using the
    horizontal layout.
    
    Either way rebalancing seems unnecessary and is also intrusive since it
    rearranges all windows, not just those the plugin uses.
    The call in `Sidebar:close()` is removed for the same reason.

--- lua/avante/sidebar.lua
+++ lua/avante/sidebar.lua
@@ -127,7 +127,6 @@ function Sidebar:open(opts)
     vim.g.avante_login = true
   end
 
-  vim.cmd("wincmd =")
   return self
 end
 
@@ -144,8 +143,6 @@ function Sidebar:close(opts)
   if opts.goto_code_win and self.code and self.code.winid and api.nvim_win_is_valid(self.code.winid) then
     fn.win_gotoid(self.code.winid)
   end
-
-  vim.cmd("wincmd =")
 end
 
 function Sidebar:shutdown()
