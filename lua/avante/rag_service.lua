8918842e9cb759e0420451c30c046278eaa9a0a0
yetone <yetoneful@gmail.com>
2025-03-22 14:38:28 +0800

    feat: upgrade rag service image version (#1673)

--- lua/avante/rag_service.lua
+++ lua/avante/rag_service.lua
@@ -8,7 +8,7 @@ local M = {}
 local container_name = "avante-rag-service"
 local service_path = "/tmp/" .. container_name
 
-function M.get_rag_service_image() return "quay.io/yetoneful/avante-rag-service:0.0.9" end
+function M.get_rag_service_image() return "quay.io/yetoneful/avante-rag-service:0.0.10" end
 
 function M.get_rag_service_port() return 20250 end
 
