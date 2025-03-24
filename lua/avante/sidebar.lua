368558fd6d576be9bc480e8643bbf0e22c33acd4
yetone <yetoneful@gmail.com>
2025-03-24 15:41:20 +0800

    fix: special handling wehn vim diff count_a is 0 (#1692)

--- lua/avante/sidebar.lua
+++ lua/avante/sidebar.lua
@@ -974,7 +974,7 @@ local function minimize_snippet(original_lines, snippet)
     local start_a, count_a, start_b, count_b = unpack(hunk)
     ---@type AvanteCodeSnippet
     local new_snippet = {
-      range = { start_line + start_a - 1, start_line + start_a + count_a - 2 },
+      range = { start_line + start_a - 1, start_line + start_a + math.max(count_a, 1) - 2 },
       content = table.concat(vim.list_slice(snippet_lines, start_b, start_b + count_b - 1), "\n"),
       lang = snippet.lang,
       explanation = snippet.explanation,
