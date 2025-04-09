04336913b32f66f45b20a6ba2f033dd742f04374
yetone <yetoneful@gmail.com>
2025-04-09 16:58:54 +0800

    Revert "fix max_tokens for reasoning models (#1819)" (#1839)
    * Revert "fix max_tokens for reasoning models (#1819)"
    
    This reverts commit 1e2e233ff5a1ff92ede4d2631746f66889424faa.
    
    * Revert "fix: revert max_completion_tokens to max_tokens (#1741)"
    
    This reverts commit cd13eeb7d9093b74b14da7382d49addbf9846f04.
    
    * fix: nvim_version

--- lua/avante/providers/openai.lua
+++ lua/avante/providers/openai.lua
@@ -67,13 +67,8 @@ function M.set_allowed_params(provider_conf, request_body)
   else
     request_body.reasoning_effort = nil
   end
-
-  if M.is_reasoning_model(provider_conf.model) then
-    if request_body.max_tokens then
-      request_body.max_completion_tokens = request_body.max_tokens
-      request_body.max_tokens = nil
-    end
-  end
+  -- If max_tokens is set in config, unset max_completion_tokens
+  if request_body.max_tokens then request_body.max_completion_tokens = nil end
 end
 
 function M:parse_messages(opts)
