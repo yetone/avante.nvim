422908caf92f3fe28dcb21e5ade61a0569738b70
doodleEsc <cinuor@gmail.com>
2025-05-05 17:59:54 +0800

    refactor: clean up extensions class
    - Remove unused fields from the extensions class definition
    
    Signed-off-by: doodleEsc <cinuor@gmail.com>

--- lua/avante/extensions/init.lua
+++ lua/avante/extensions/init.lua
@@ -1,10 +1,4 @@
 ---@class avante.extensions
----@field tokens avante.utils.tokens
----@field root avante.utils.root
----@field file avante.utils.file
----@field history avante.utils.history
----@field environment avante.utils.environment
----@field lsp avante.utils.lsp
 local M = {}
 
 setmetatable(M, {
