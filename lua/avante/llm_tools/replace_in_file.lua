914fa72813c67e132169b83bbcae9a1eb8e5135b
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2025-07-24 21:17:32 -0700

    cleanup: stop referencing vim.highlights
    vim.hl was introduced in neovim 0.8 and vim.highlights is being
    deprecated. Given that Avante requires neovim 0.10+ stop trying to
    fallback to the old name.

--- lua/avante/llm_tools/replace_in_file.lua
+++ lua/avante/llm_tools/replace_in_file.lua
@@ -4,7 +4,7 @@ local Utils = require("avante.utils")
 local Highlights = require("avante.highlights")
 local Config = require("avante.config")
 
-local PRIORITY = (vim.hl or vim.highlight).priorities.user
+local PRIORITY = vim.hl.priorities.user
 local NAMESPACE = vim.api.nvim_create_namespace("avante-diff")
 local KEYBINDING_NAMESPACE = vim.api.nvim_create_namespace("avante-diff-keybinding")
 
