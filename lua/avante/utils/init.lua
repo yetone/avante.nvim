f077db82d9c25e734c51698f8c331844b8e75a8b
yetone <yetoneful@gmail.com>
2025-08-31 21:32:06 +0800

    feat: supports full view in multi-window mode

--- lua/avante/utils/init.lua
+++ lua/avante/utils/init.lua
@@ -1716,4 +1716,9 @@ function M.get_unified_diff(text1, text2, opts)
   return vim.diff(text1, text2, opts)
 end
 
+function M.is_floating_window(win_id)
+  local config = vim.api.nvim_win_get_config(win_id or 0)
+  return config.relative ~= ""
+end
+
 return M
