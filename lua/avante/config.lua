375f02bd9f9e3c1d303d986a02aa23f8f4797258
teto <886074+teto@users.noreply.github.com>
2026-01-29 18:54:46 +0100

    chore: fix annotation
    to remove ambiguity that triggers error with emmylua ("lua-ls" -like)
    https://github.com/EmmyLuaLs/emmylua-analyzer-rust/issues/932

--- lua/avante/config.lua
+++ lua/avante/config.lua
@@ -47,9 +47,9 @@ M._defaults = {
   -- For most providers that we support we will determine this automatically.
   -- If you wish to use a given implementation, then you can override it here.
   tokenizer = "tiktoken",
-  ---@type string | fun(): string | nil
+  ---@type string | nil | fun(): string
   system_prompt = nil,
-  ---@type string | fun(): string | nil
+  ---@type string | nil | fun(): string
   override_prompt_dir = nil,
   rules = {
     project_dir = nil, ---@type string | nil (could be relative dirpath)
