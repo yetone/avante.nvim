ea06bb7fd5222d9e22eb4cf2337d22c845fe7451
teto <886074+teto@users.noreply.github.com>
2026-05-28 15:37:28 +0200

    refactor!: move some of the sidebar setup code to ftplugin/
    makes the various filetypes used easier to discover (just peek at
    ftplugin/) and also more overridable since the user can now just create
    its own ftplugin/AvanteInput.lua to override avante's behavior.
    
    I hope we can generalize this change to the other filetypes as well.

---
+++ ftplugin/AvanteInput.lua
@@ -0,0 +1,75 @@
+-- create your own ftplugin/Avante.lua or after/ftplugin/Avante.lua to alter behavior
+local api = vim.api
+local Utils = require("avante.utils")
+local Config = require("avante.config")
+
+local sidebar = require("avante").get()
+
+-- Setup completion
+api.nvim_create_autocmd("InsertEnter", {
+  group = sidebar.augroup,
+  buffer = sidebar.containers.input.bufnr,
+  once = true,
+  desc = "Setup the completion of helpers in the input buffer",
+  callback = function() end,
+})
+
+local debounced_show_input_hint = Utils.debounce(function()
+  if vim.api.nvim_win_is_valid(sidebar.containers.input.winid) then sidebar:show_input_hint() end
+end, 200)
+api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "VimResized" }, {
+  group = sidebar.augroup,
+  buffer = sidebar.containers.input.bufnr,
+  callback = function()
+    debounced_show_input_hint()
+    sidebar.place_sign_at_first_line(sidebar.containers.input.bufnr)
+  end,
+})
+
+api.nvim_create_autocmd("QuitPre", {
+  group = sidebar.augroup,
+  buffer = sidebar.containers.input.bufnr,
+  callback = function() sidebar:close_input_hint() end,
+})
+
+api.nvim_create_autocmd("WinClosed", {
+  group = sidebar.augroup,
+  pattern = tostring(sidebar.containers.input.winid),
+  callback = function() sidebar:close_input_hint() end,
+})
+
+api.nvim_create_autocmd("BufEnter", {
+  group = sidebar.augroup,
+  buffer = sidebar.containers.input.bufnr,
+  callback = function()
+    if Config.windows.ask.start_insert then vim.cmd("noautocmd startinsert!") end
+  end,
+})
+
+api.nvim_create_autocmd("BufLeave", {
+  group = sidebar.augroup,
+  buffer = sidebar.containers.input.bufnr,
+  callback = function()
+    vim.cmd("noautocmd stopinsert")
+    sidebar:close_input_hint()
+  end,
+})
+
+-- Update hint on mode change as submit key sequence may be different
+api.nvim_create_autocmd("ModeChanged", {
+  group = sidebar.augroup,
+  buffer = sidebar.containers.input.bufnr,
+  callback = function() sidebar:show_input_hint() end,
+})
+
+api.nvim_create_autocmd("WinEnter", {
+  group = sidebar.augroup,
+  callback = function()
+    local cur_win = api.nvim_get_current_win()
+    if sidebar.containers.input and cur_win == sidebar.containers.input.winid then
+      sidebar:show_input_hint()
+    else
+      sidebar:close_input_hint()
+    end
+  end,
+})
