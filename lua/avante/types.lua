2b6c9f95f167cfe3507a5b4c9023dbcfc72ab410
teto <886074+teto@users.noreply.github.com>
2026-05-28 17:14:59 +0200

    chore: address lua-language-server "variable reuse" warnings

--- lua/avante/types.lua
+++ lua/avante/types.lua
@@ -118,7 +118,7 @@ vim.g.avante_login = vim.g.avante_login
 ---@field turn_id string | nil
 ---@field is_calling boolean | nil
 ---@field original_content AvanteLLMMessageContent | nil
----@field acp_tool_call? avante.acp.ToolCall
+---@field acp_tool_call? avante.acp.ToolCall | avante.acp.ToolCallUpdate
 
 ---@class AvanteLLMToolResult
 ---@field tool_name string
