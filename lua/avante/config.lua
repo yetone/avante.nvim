9f0462c9cfbcd09b5288a07b153284fded2ce2ee
teto <886074+teto@users.noreply.github.com>
2026-06-29 14:09:39 +0200

    doc: misc
    cleanup

--- lua/avante/config.lua
+++ lua/avante/config.lua
@@ -268,10 +268,10 @@ end
 ---@field prev_prompt? avante.Config.PromptLoggerKeymap Mapping used to load the previous older prompt log.
 
 ---@class avante.Config.RagServiceModel
----@field provider string Model provider.
----@field endpoint string Model API endpoint.
+---@field provider string Model provider. One available in py/rag-service/src/providers ie., "ollama", "openai", "dashscope", "openrouter"
+---@field endpoint string Model API endpoint (e.g., http://localhost)
 ---@field api_key string Environment variable name for the model API key.
----@field model string Model name.
+---@field model string Model name
 ---@field extra? table Additional model provider options.
 
 ---@class avante.Config.RagService
@@ -385,8 +385,8 @@ M._defaults = {
     llm = { -- Configuration for the Language Model (LLM) used by the RAG service
       provider = "openai", -- The LLM provider
       endpoint = "https://api.openai.com/v1", -- The LLM API endpoint
-      api_key = "OPENAI_API_KEY", -- The environment variable name for the LLM API key
-      model = "gpt-4o-mini", -- The LLM model name
+      api_key = "OPENAI_API_KEY",
+      model = "gpt-4o-mini",
       extra = nil, -- Extra configuration options for the LLM
     },
     embed = { -- Configuration for the Embedding model used by the RAG service
